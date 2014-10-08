d60e61496262b195c1d46d2aabb3fbd5abf5afde
David Balatero <dbalatero@gmail.com>
2014-10-08 09:45:03 -0700

    Add a custom rake routes alias

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -3,6 +3,8 @@ ms() { bash -c "cd ~/code/mariners && ./scoreboard $@" }
 alias dbm="rake db:migrate"
 alias ole="bin/open_last_error"
 
+zroute() { zeus rake routes | ag $@ }
+
 ctags_refresh() {
   ctags -R .
   ctags -R -f ./.git/tags
