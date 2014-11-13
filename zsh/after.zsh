fdfd82b36ef99709c88e8f51028ba3205049e18f
David Balatero <dbalatero@gmail.com>
2014-11-13 12:59:10 -0800

    nvm

--- zsh/after.zsh
+++ zsh/after.zsh
@@ -1,2 +1,5 @@
 export PATH="$HOME/.rvm/bin:$PATH" # Add RVM to PATH for scripting
 source ~/.rvm/scripts/rvm
+
+export NVM_DIR="/Users/dbalatero/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
