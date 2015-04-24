17082a9f7e0766f1fca5d5ec2dcf04534169db28
David Balatero <dbalatero@gmail.com>
2015-04-24 16:02:45 -0700

    NVM hardcoded my username

--- zsh/after.zsh
+++ zsh/after.zsh
@@ -3,5 +3,5 @@ eval "$(direnv hook zsh)"
 export PATH="$HOME/.rvm/bin:$PATH" # Add RVM to PATH for scripting
 source ~/.rvm/scripts/rvm
 
-export NVM_DIR="/Users/dbalatero/.nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
