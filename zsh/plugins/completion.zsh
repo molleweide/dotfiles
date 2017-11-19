81f8be6b0f8e681db1c1a283ae17c092e794f080
David Balatero <dbalatero@gmail.com>
2017-11-18 19:29:41 -0800

    Move things to plugins dir

---
+++ zsh/plugins/completion.zsh
@@ -0,0 +1,9 @@
+fpath=(/usr/local/share/zsh-completions $fpath)
+
+# The following lines were added by compinstall
+zstyle ':completion:*' completer _complete _ignored
+zstyle :compinstall filename '/Users/dbalatero/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
