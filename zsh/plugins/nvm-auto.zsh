7dd5fa951ae86a2ebd37d7f36074b9c536a93601
David Balatero <dbalatero@gmail.com>
2017-11-25 14:38:10 -0800

    Fix zshrc

--- zsh/plugins/nvm-auto.zsh
+++ zsh/plugins/nvm-auto.zsh
@@ -5,13 +5,18 @@ nvm_auto_switch() {
   local REAL_VERSION
   local NVMRC_LOCATION=$(nvm_find_nvmrc)
 
-  if [ "$NVMRC_LOCATION" == "" ]; then
+  if [[ $NVMRC_LOCATION = "" ]]; then
     return 0
   fi
 
   read NVM_RC_VERSION < $(nvm_find_nvmrc)
-  REAL_VERSION=$(nvm_version $NVM_RC_VERSION) 
-  [ "$(nvm_version_path $REAL_VERSION)/bin" == "$NVM_BIN" ] || nvm use
+
+  REAL_VERSION=$(nvm_version $NVM_RC_VERSION)
+
+  [[ "$(nvm_version_path $REAL_VERSION)/bin" != "$NVM_BIN" ]] && nvm use
 }
 
-add-zsh-hook -Uz chpwd (){ nvm_auto_switch; }
+chpwd_functions+=('nvm_auto_switch')
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
--- zshrc
+++ zshrc
@@ -37,13 +37,7 @@ do
   source $file
 done
 
-eval "$(direnv hook zsh)"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+# Add RVM to PATH for scripting. Make sure this is the last
+# PATH variable change.
 export PATH="$PATH:$HOME/.rvm/bin"
 [ -f ~/.rvm/scripts/rvm ] && source ~/.rvm/scripts/rvm
