d04f69d2e37e1875be2ea31dae113ee7feab144b
David Balatero <dbalatero@gmail.com>
2017-11-25 13:29:31 -0800

    More installers

--- install.sh
+++ install.sh
@@ -3,21 +3,16 @@
 installers="${BASH_SOURCE%/*}/installers"
 
 $installers/homebrew
+$installers/git
+$installers/devtools
 $installers/zsh
+$installers/ruby
+$installers/tmux
+$installers/fonts
 
 brew install neovim/neovim/neovim --HEAD
-brew install phantomjs
-brew install tmux
 brew install pyenv
 brew install --HEAD pyenv-virtualenv
-brew install ctags
-
-if [ ! -f $HOME/.fzf.zsh ]; then
-  /usr/local/opt/fzf/install
-fi
-
-brew tap caskroom/fonts
-brew cask install font-hack-nerd-font
 
 export PYTHON_CONFIGURE_OPTS="--enable-framework"
 
@@ -27,11 +22,6 @@ eval "$(pyenv virtualenv-init -)"
 pyenv install -s 2.7.13
 pyenv install -s 3.6.2
 pyenv global 3.6.2
-
-if [ ! -d ~/.config/base16-shell ]; then
-  git clone git clone https://github.com/chriskempson/base16-shell.git \
-    ~/.config/base16-shell
-fi
 
 if [ ! -d $(pyenv root)/plugins/pyenv-virtualenv ]; then
   git clone https://github.com/yyuu/pyenv-virtualenv.git \
@@ -52,101 +42,6 @@ pip install neovim
 pyenv activate py3neovim
 pip install neovim
 
-if [ ! -d ~/.dotfiles/dependencied/fonts ]; then
-  echo "Setting up patched Powerline fonts..."
-  git clone git@github.com:powerline/fonts.git ~/.dotfiles/dependencies/fonts
-  ~/.dotfiles/dependencies/fonts/install.sh
-fi
-
-nerd_fonts_dir=$HOME/.dotfiles/dependencies/patched-nerd-fonts
-
-if [ ! -d $nerd_fonts_dir ]; then
-  echo "Pulling in patched nerd-fonts..."
-
-  mkdir -p $nerd_fonts_dir
-
-  cd $nerd_fonts_dir && \
-    git init && \
-    mkdir .git/info && \
-    git remote add origin https://github.com/ryanoasis/nerd-fonts.git && \
-    git config core.sparsecheckout true && \
-    echo "patched-fonts/*" >> .git/info/sparse-checkout && \
-    git pull --depth=1 origin master
-
-  cp $nerd_fonts_dir/**/*.ttf $HOME/Library/Fonts
-fi
-
-if [ ! -f ~/.rvmrc ]; then
-  ln -s ~/.dotfiles/rvmrc ~/.rvmrc
-fi
-
-if [ ! -f ~/.gitconfig.user ]; then
-  printf "What is your GitHub username? > "
-  read github_user
-  git config -f ~/.gitconfig.user github.user "$github_user"
-
-  printf "What is your git name? > "
-  read git_name
-  git config -f ~/.gitconfig.user user.name "$git_name"
-
-  printf "What is your git email? > "
-  read git_email
-  git config -f ~/.gitconfig.user user.email "$git_email"
-fi
-
-
-echo "Setting up git"
-if [ ! -f ~/.gitconfig ]; then
-  ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-fi
-
-if [ ! -f ~/.gitignore ]; then
-  ln -s ~/.dotfiles/git/gitignore ~/.gitignore
-fi
-
-if [ ! -d ~/.git_template ]; then
-  ln -s ~/.dotfiles/git/template ~/.git_template
-fi
-
-echo "Setting up ctags"
-if [ ! -f ~/.ctags ]; then
-  ln -s ~/.dotfiles/ctags ~/.ctags
-fi
-
-echo "Setting up pry"
-if [ ! -f ~/.pryrc ]; then
-  ln -s ~/.dotfiles/irb/pryrc ~/.pryrc
-fi
-
-if [ ! -f ~/.unescaped_colors.rb ]; then
-  ln -s ~/.dotfiles/irb/unescaped_colors.rb ~/.unescaped_colors.rb
-fi
-
-if [ ! -d ~/.tmuxinator ]; then
-  echo "Setting up Tmuxinator"
-  ln -s ~/.dotfiles/tmuxinator ~/.tmuxinator
-fi
-
-if [ ! -f ~/.escaped_colors.rb ]; then
-  ln -s ~/.dotfiles/irb/escaped_colors.rb ~/.escaped_colors.rb
-fi
-
-if [ ! -f ~/.aprc ]; then
-  ln -s ~/.dotfiles/irb/aprc ~/.aprc
-fi
-
-echo "Setting up tmux"
-if [ ! -f ~/.tmux.conf ]; then
-  ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-fi
-
-if [ ! -d ~/.tmux ]; then
-  ln -s ~/.dotfiles/tmux ~/.tmux
-fi
-
-if [ ! -d ~/.tmux/plugins/tpm ]; then
-  git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
-fi
 
 if [ ! -d ~/.config ]; then
   mkdir -p ~/.config
--- installers/shared/functions.sh
+++ installers/shared/functions.sh
@@ -18,7 +18,7 @@ function brew_install_all() {
   done
 }
 
-function install_location() {
+function dotfiles_location() {
   local current_dir="${BASH_SOURCE%/*}"
   echo $(cd $current_dir/../.. && pwd)
 }
@@ -26,7 +26,7 @@ function install_location() {
 function symlink_dotfile() {
   local file=$1
   local destination=$2
-  local full_file_path="$(install_location)/$file"
+  local full_file_path="$(dotfiles_location)/$file"
 
   if [ ! -e $destination ]; then
     echo "Symlinking $full_file_path -> $destination"
