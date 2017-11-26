35436804e7e0725bb74c7d5efca2b9cd52cdfcf5
David Balatero <dbalatero@gmail.com>
2017-11-26 12:32:31 -0800

    Fix homebrew

--- zsh/custom/exports.zsh
+++ zsh/custom/exports.zsh
@@ -3,7 +3,7 @@ export EDITOR=nvim
 export NVIM_TUI_ENABLE_TRUE_COLOR=1
 
 # Path
-export PATH="$HOME/.local/bin:/usr/local/bin:$PATH"
+export PATH="$HOME/.local/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export PATH="$HOME/.yarn/bin:$PATH"
 export PATH="$PATH:`yarn global bin`"
 export PATH="$PATH:$HOME/.composer/vendor/bin"
