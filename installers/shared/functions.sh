db09fb12b882ea94da90acd560a35c020a8630dd
David Balatero <dbalatero@gmail.com>
2017-11-25 16:49:48 -0800

    Add headers, colors

--- installers/shared/functions.sh
+++ installers/shared/functions.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 
+dotheader() {
+  dotsay "@b@green[[$1]]"
+}
+
+dotsay() {
+  local result=$(_colorized $@)
+  echo "$result"
+}
+
+_colorized() {
+   echo "$@" | sed -E \
+     -e 's/((@(red|green|yellow|blue|magenta|cyan|white|reset|b|u))+)[[]{2}(.*)[]]{2}/\1\4@reset/g' \
+     -e "s/@red/$(tput setaf 1)/g" \
+     -e "s/@green/$(tput setaf 2)/g" \
+     -e "s/@yellow/$(tput setaf 3)/g" \
+     -e "s/@blue/$(tput setaf 4)/g" \
+     -e "s/@magenta/$(tput setaf 5)/g" \
+     -e "s/@cyan/$(tput setaf 6)/g" \
+     -e "s/@white/$(tput setaf 7)/g" \
+     -e "s/@reset/$(tput sgr0)/g" \
+     -e "s/@b/$(tput bold)/g" \
+     -e "s/@u/$(tput sgr 0 1)/g"
+}
+
 function brew_install() {
   local package=$1
 
   if brew list "$package" > /dev/null 2>&1; then
-    echo "$package already installed... skipping."
+    dotsay "+ $package already installed... skipping."
   else
     brew install $@
   fi
@@ -39,4 +63,7 @@ function require_installer() {
   local installer_path="$(dotfiles_location)/installers/$name"
 
   $installer_path
+
+  dotsay "@b@blue[[+ Done!]]"
+  echo
 }
