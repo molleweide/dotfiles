4e20e3d1aec817dea5b3cecc714371d4d53e035d
David Balatero <dbalatero@gmail.com>
2018-01-21 10:36:28 -0800

    Set cursor colors to base16 theme in evil modes

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -381,6 +381,7 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   (require 'init-origami)
+  (require 'init-base16-evil)
 
   (add-hook 'prog-mode-hook 'rainbow-mode)
 
