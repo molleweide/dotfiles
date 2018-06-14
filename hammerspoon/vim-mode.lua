0b4cf718090441601cf280b3fbac0accfaa7c4bb
David Balatero <dbalatero@gmail.com>
2018-06-14 13:14:01 -0700

    Add screen dim, hjkl

--- hammerspoon/vim-mode.lua
+++ hammerspoon/vim-mode.lua
@@ -1,6 +1,13 @@
 local vimMode = hs.hotkey.modal.new(nil, nil, 'in vim mode')
 local enterVimMode = hs.hotkey.modal.new({'shift'}, 'J', 'possibly entering')
 
+local function dimScreen()
+  hs.screen.primaryScreen():setGamma(
+    {alpha=1.0,red=0.0,green=0.0,blue=0.0},
+    {blue=0.0,green=1.0,red=0.0}
+  )
+end
+
 function enterVimMode:entered()
   hs.timer.doAfter(100 / 1000, function()
     enterVimMode:exit()
@@ -11,18 +18,24 @@ enterVimMode:bind({'shift'}, 'K', function() vimMode:enter() end)
 
 function vimMode:entered()
   hs.alert.show("vim mode")
+  dimScreen()
   enterVimMode:exit()
 end
 
 function vimMode:exited()
   hs.notify.new({title="Hammerspoon", informativeText="Exiting vim mode"}):send()
+  hs.screen.restoreGamma()
 end
 
 local function keyCode(key, modifiers)
   modifiers = modifiers or {}
-  return function() hs.eventtap.keyStroke(modifiers, key) end
+  return function() hs.eventtap.keyStroke(modifiers, key, 0) end
 end
 
+vimMode:bind({}, 'h', keyCode('left'), nil, keyCode('left'))
+vimMode:bind({}, 'j', keyCode('down'), nil, keyCode('down'))
+vimMode:bind({}, 'k', keyCode('up'), nil, keyCode('up'))
+vimMode:bind({}, 'l', keyCode('right'), nil, keyCode('right'))
 
 vimMode:bind({}, '0', keyCode('left', {'command'}), nil, keyCode('left', {'command'}))
 vimMode:bind({'shift'}, '4', keyCode('right', {'command'}), nil, keyCode('right', {'command'}))
