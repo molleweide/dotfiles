d1686fc990aee9c74fe08361736e69af3d373986
David Balatero <dbalatero@gmail.com>
2019-04-09 11:03:32 -0700

    update prompt

--- zsh/custom/theme.zsh
+++ zsh/custom/theme.zsh
@@ -3,4 +3,21 @@ setopt prompt_subst
 autoload -U promptinit
 promptinit
 
+SPACESHIP_PROMPT_ORDER=(
+  dir           # Current directory section
+  git           # Git section (git_branch + git_status)
+  node          # Node.js section
+  ruby          # Ruby section
+  elixir        # Elixir section
+  exec_time     # Execution time
+  line_sep      # Line break
+  battery       # Battery level and status
+  vi_mode       # Vi-mode indicator
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+
+SPACESHIP_NODE_SHOW=true
+
 prompt spaceship
