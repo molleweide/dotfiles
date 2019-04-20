4efd1ba0a07b949388e2212d8c023a97ae63377a
David Balatero <dbalatero@gmail.com>
2019-04-20 10:51:00 -0700

    Fix luacheck warnings

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,12 +6,10 @@ require "key-bindings"
 require "mute-on-sleep"
 require "audio-switcher"
 
-vim = hs.loadSpoon('VimMode')
+local vim = hs.loadSpoon('VimMode')
 
 vim:disableForApp('zoom.us')
 vim:disableForApp('iTerm')
 vim:disableForApp('iTerm2')
 
 vim:enableKeySequence('j', 'k')
-
-Install = spoon.SpoonInstall
