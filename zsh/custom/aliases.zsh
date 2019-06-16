7f85cc41ac0b5536a96e373e6311f7cb3c7b93de
David Balatero <dbalatero@gmail.com>
2019-06-16 09:26:33 -0700

    Tighten up aliases file

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,28 +1,20 @@
-alias vim="nvim"
-
-alias l="ls -al"
-alias tn="rvm . do tmuxinator"
-alias tk="tmux kill-session"
-alias work="cd ~/code/boundless && tn start boundless"
-alias word="cd ~/code/boundless && tn start wordpress"
-alias mobile="cd ~/code/boundless && tn start mobile"
-alias dbm="rake db:migrate && RAILS_ENV=test rake db:migrate"
-alias less="less -r"
-
+# Shell aliases
 alias b="aid begin"
-alias bs="./bin/rspec"
-alias bat="open -a 'Adobe Acrobat' $@"
-
-alias md="open -a Markoff $@"
-
-# Simple rsync
+alias dbm="rake db:migrate && RAILS_ENV=test rake db:migrate"
+alias l="ls -al"
+alias j=z # I'm used to autojump 'j' vs fasd 'z'
+alias less="less -r"
+alias sketch="magick $1 \( -clone 0 -negate -blur 0x5 \) \
+  -compose colordodge -composite -modulate 100,0,100 -auto-level $2"
 alias srync="rsync -vrazh"
+alias tk="tmux kill-session"
+alias tn="rvm . do tmuxinator"
+alias vim="nvim"
+alias work="cd ~/code/boundless && tn start boundless"
 
-# I'm used to autojump 'j' vs fasd 'z'
-alias j=z
-
-alias sketch="magick $1 \( -clone 0 -negate -blur 0x5 \) -compose colordodge -composite \
--modulate 100,0,100 -auto-level $2"
+# OS X apps
+alias bat="open -a 'Adobe Acrobat' $@"
+alias md="open -a Markoff $@"
 
 pgrefresh() {
   rm -fr /usr/local/var/postgres/postmaster.pid
@@ -30,5 +22,7 @@ pgrefresh() {
 }
 
 sslcert() {
-  echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | openssl x509 -inform pem -noout -text
+  echo | \
+    openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | \
+    openssl x509 -inform pem -noout -text
 }
