ee8b24540cbc80dadc600e537617f122d568315d
David Balatero <dbalatero@gmail.com>
2019-06-16 09:27:04 -0700

    80 chars

--- zsh/custom/exports.zsh
+++ zsh/custom/exports.zsh
@@ -1,5 +1,6 @@
 # FZF
-export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git --exclude node_modules --color=always'
+export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden \
+--exclude .git --exclude node_modules --color=always'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS="--ansi"
 
