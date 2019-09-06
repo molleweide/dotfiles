35f4bd55c0946f4cb24de3407f721e9aacc07190
David Balatero <dbalatero@gmail.com>
2019-09-06 13:26:17 -0700

    Fix the Slack thread hotkey

--- hammerspoon/slack.lua
+++ hammerspoon/slack.lua
@@ -1,10 +1,10 @@
 local function jumpToSlackThread()
-  hs.eventtap.keyStroke({'shift'}, 'tab', 0)
-  hs.eventtap.keyStroke({'shift'}, 'tab', 0)
-  hs.eventtap.keyStroke({'shift'}, 'tab', 0)
-  hs.eventtap.keyStroke({'shift'}, 'tab', 0)
-  hs.eventtap.keyStroke({'shift'}, 'tab', 0)
-  hs.eventtap.keyStroke({'shift'}, 'tab', 0)
+  hs.eventtap.keyStroke({'shift'}, 'tab', 500)
+  hs.eventtap.keyStroke({'shift'}, 'tab', 500)
+  hs.eventtap.keyStroke({'shift'}, 'tab', 500)
+  hs.eventtap.keyStroke({'shift'}, 'tab', 500)
+  hs.eventtap.keyStroke({'shift'}, 'tab', 500)
+  hs.eventtap.keyStroke({'shift'}, 'tab', 500)
   hs.eventtap.keyStroke({}, 'return')
 end
 
