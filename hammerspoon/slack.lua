41eaeda66e88313d85f62269384bfe278de7b81e
David Balatero <dbalatero@gmail.com>
2019-09-06 13:40:06 -0700

    Select the message box before going backwards

--- hammerspoon/slack.lua
+++ hammerspoon/slack.lua
@@ -1,10 +1,16 @@
 local function jumpToSlackThread()
+  -- select the message window
+  hs.eventtap.keyStroke({'shift'}, 'F6', 500)
+
+  -- tab backwards to the thread button
   hs.eventtap.keyStroke({'shift'}, 'tab', 500)
   hs.eventtap.keyStroke({'shift'}, 'tab', 500)
   hs.eventtap.keyStroke({'shift'}, 'tab', 500)
   hs.eventtap.keyStroke({'shift'}, 'tab', 500)
   hs.eventtap.keyStroke({'shift'}, 'tab', 500)
   hs.eventtap.keyStroke({'shift'}, 'tab', 500)
+
+  -- click it
   hs.eventtap.keyStroke({}, 'return')
 end
 
