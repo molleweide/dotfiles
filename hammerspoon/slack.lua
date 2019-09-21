915644c5b608a7515dd4f4fafc15828a0a800c53
David Balatero <dbalatero@gmail.com>
2019-09-21 08:33:44 -0700

    Add more slack shortcuts

--- hammerspoon/slack.lua
+++ hammerspoon/slack.lua
@@ -18,5 +18,21 @@ local function jumpToSlackThread()
   hs.eventtap.keyStroke({}, 'return')
 end
 
+local function slackEmojiReact(name)
+  hs.eventtap.keyStroke({'cmd', 'shift'}, '\\', 500)
+
+  for i = 1, #name do
+    char = string.sub(name, i, i)
+    hs.eventtap.keyStroke({}, char, 500)
+  end
+
+  -- hs.eventtap.keyStroke({}, 'return', 10000)
+end
+
+local function emojiBomb()
+  slackEmojiReact('avocado')
+end
+
+hs.hotkey.bind(hyper, 'e', emojiBomb)
 hs.hotkey.bind(hyper, 'f', focusSlackMessageBox)
 hs.hotkey.bind(hyper, 't', jumpToSlackThread)
