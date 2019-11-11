c1dc55eff14d3c726d1e43bd7e4e13be24c9105f
David Balatero <dbalatero@gmail.com>
2019-11-11 10:21:53 -0800

    Slack thread open

--- hammerspoon/slack.lua
+++ hammerspoon/slack.lua
@@ -11,8 +11,6 @@ local function jumpToSlackThread()
   hs.eventtap.keyStroke({'shift'}, 'tab', 500)
   hs.eventtap.keyStroke({'shift'}, 'tab', 500)
   hs.eventtap.keyStroke({'shift'}, 'tab', 500)
-  hs.eventtap.keyStroke({'shift'}, 'tab', 500)
-  hs.eventtap.keyStroke({'shift'}, 'tab', 500)
 
   -- click it
   hs.eventtap.keyStroke({}, 'return')
