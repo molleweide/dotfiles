873bc2900740e5374b79286e333ffadc2391713d
David Balatero <dbalatero@gmail.com>
2020-05-19 08:15:02 -0400

    Add cheat command

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -38,3 +38,7 @@ function sslcert() {
     openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | \
     openssl x509 -inform pem -noout -text
 }
+
+function cheat() {
+  curl cht.sh/$1
+}
