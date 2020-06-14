9fda48e0e8149225f40875ffbe0acec94a815c04
David Balatero <dbalatero@gmail.com>
2020-06-13 20:53:17 -0400

    Switch terminals to Alacritty

--- hammerspoon/quick-switch.lua
+++ hammerspoon/quick-switch.lua
@@ -2,7 +2,7 @@
 quickSwitchBindings = {
   {hyper, '1', '/Applications/1Password 7.app'},
   {hyper, 'c', '/Applications/Google Chrome.app'},
-  {hyper, 'i', '/Applications/iTerm.app'},
+  {hyper, 'i', '/Applications/Alacritty.app'},
   {hyper, 'l', '/Applications/Slack.app'},
   {hyper, 'm', '/Applications/Messages.app'},
   {hyper, 's', '/Applications/Spotify.app'},
