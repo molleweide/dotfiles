5ad86dc353bcd7704bb2c272710ba372d2e6677b
David Balatero <dbalatero@gmail.com>
2020-07-15 11:58:12 -0400

    Fix utf8

--- hammerspoon/github.lua
+++ hammerspoon/github.lua
@@ -1,25 +1,3 @@
-local function fixUTF8(s, replacement)
-  local p, len, invalid = 1, #s, {}
-
-  while p <= len do
-    if     p == s:find("[%z\1-\127]", p) then p = p + 1
-    elseif p == s:find("[\194-\223][\128-\191]", p) then p = p + 2
-    elseif p == s:find(       "\224[\160-\191][\128-\191]", p)
-        or p == s:find("[\225-\236][\128-\191][\128-\191]", p)
-        or p == s:find(       "\237[\128-\159][\128-\191]", p)
-        or p == s:find("[\238-\239][\128-\191][\128-\191]", p) then p = p + 3
-    elseif p == s:find(       "\240[\144-\191][\128-\191][\128-\191]", p)
-        or p == s:find("[\241-\243][\128-\191][\128-\191][\128-\191]", p)
-        or p == s:find(       "\244[\128-\143][\128-\191][\128-\191]", p) then p = p + 4
-    else
-      s = s:sub(1, p-1)..replacement..s:sub(p+1)
-      table.insert(invalid, p)
-    end
-  end
-
-  return s, invalid
-end
-
 local function getRichLinkToCurrentChromeTab()
   local application = hs.application.frontmostApplication()
 
@@ -28,11 +6,21 @@ local function getRichLinkToCurrentChromeTab()
     return
   end
 
-  local window = application:focusedWindow()
-  local title = string.gsub(window:title(), "- - Google Chrome.*", "")
-  title = fixUTF8(title, "")
-
   local script = [[
+    tell application "Google Chrome"
+      get title of active tab of first window
+    end tell
+  ]]
+
+  local _, title = hs.osascript.applescript(script)
+
+  -- remove trailing garbage from window title
+  title = string.gsub(title, "- - Google Chrome.*", "")
+
+  -- fix unicode issue with Github links ugh
+  title = string.gsub(title, " · ", " | ")
+
+  script = [[
     tell application "Google Chrome"
       get URL of active tab of first window
     end tell
