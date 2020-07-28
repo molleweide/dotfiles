2a5aedc4e7d32f47b882b01736397dfb2b831763
David Balatero <dbalatero@gmail.com>
2020-07-28 13:45:44 -0400

    Drop dnd toggle

--- hammerspoon/pairing-mode.lua
+++ hammerspoon/pairing-mode.lua
@@ -1,6 +1,11 @@
+local function enableDoNotDisturb()
+  hs.execute("defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean true")
+  hs.execute("defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate -date \"`date -u +\"%Y-%m-%d %H:%M:%S +000\"`\"")
+  hs.execute("killall NotificationCenter")
+end
+
 hs.hotkey.bind(hyper, 'p', function()
-  -- toggle Do Not Disturb on notification center
-  hs.execute("~/.local/bin/dnd-toggle enable")
+  enableDoNotDisturb()
 
   -- close embarrassing personal apps
   hs.execute("killall Signal")
