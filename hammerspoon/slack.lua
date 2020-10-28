a4be9fa804dae1751b36b558b7784d136221b20d
David Balatero <dbalatero@gmail.com>
2020-10-28 10:38:35 -0400

    update hs

--- hammerspoon/slack.lua
+++ hammerspoon/slack.lua
@@ -16,34 +16,3 @@ end
 
 hs.hotkey.bind(hyper, 'f', focusSlackMessageBox)
 hs.hotkey.bind(hyper, 'r', openSlackReminder)
-
-local logger = hs.logger.new('whatever', 'debug')
-
--- Fix the god damn cmd + k switcher
-local focusWatcher = nil
-
-hs.eventtap.new(
-  { hs.eventtap.event.types.keyDown },
-  function(event)
-    local keyPressed = hs.keycodes.map[event:getKeyCode()]
-    local matchesKey = keyPressed == "k" or keyPressed == "t"
-
-    if matchesKey and event:getFlags():containExactly({'cmd'}) then
-      local app = hs.application.frontmostApplication()
-
-      if app:name() == "Slack" then
-        focusWatcher = app:newWatcher(function(_, event)
-          if event == hs.uielement.watcher.focusedElementChanged then
-            -- fire a down arrow to highlight the first item after
-            -- the box pops up.
-            hs.eventtap.keyStroke({}, 'down', 0)
-            focusWatcher:stop()
-          end
-        end):start({ hs.uielement.watcher.focusedElementChanged })
-      end
-    end
-
-    -- pass through key strokes
-    return false
-  end
-):start()
