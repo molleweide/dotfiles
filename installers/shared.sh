f2ec140b4343d9d7026244bf722e7c3836f9bd7f
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-09-02 18:40:52 +0200

    move bin to local; improve symlinks func

--- installers/shared.sh
+++ installers/shared.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-# NOTE: BASH_SOURCE | https://stackoverflow.com/questions/35006457/choosing-between-0-and-bash-source
-# NOTE: An array variable whose members are the source filenames corresponding to the elements in the FUNCNAME array variable.
-# NOTE: this is how it is used currently in installer scripts
-# NOTE: ```source "${BASH_SOURCE%/*}/shared.sh"```
+# FUNCNAME | https://stackoverflow.com/questions/1835943/how-to-determine-function-name-from-inside-a-function
+#   An array variable containing the names of all shell functions currently in
+#   the execution call stack.   The  element  with index  0 is the name of any
+#   currently-executing shell function.  The bottom-most element is "main".
+#   This variable exists only when a shell function is executing.  Assignments
+#   to FUNCNAME have no effect and return an error status.  If FUNCNAME is
+#   unset, it loses its special properties, even if it is subsequently reset.
+
+
+# BASH_SOURCE | https://stackoverflow.com/questions/35006457/choosing-between-0-and-bash-source
+#   An array variable whose members are the source filenames corresponding
+#   to the elements in the FUNCNAME array variable.
+#   ```source "${BASH_SOURCE%/*}/shared.sh"```
 
 shared_dir="${BASH_SOURCE%/*}/shared"
 
