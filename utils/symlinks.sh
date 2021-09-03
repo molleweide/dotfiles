fdd1e7fc38b48c0fb0fe7fcc4cd3efb9bd8fdf0d
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-09-03 07:59:06 +0200

    utils module

--- utils/symlinks.sh
+++ utils/symlinks.sh
@@ -7,17 +7,35 @@
 # }
 
 function dotfiles_location() {
-  echo "$HOME/.dotfiles"
+    echo "$HOME/.dotfiles"
 }
 
-function symlink_dotfile() {
-  local file=$1
-  local destination=$2
-  local full_file_path="$(dotfiles_location)/$file"
+# function symlink_dotfile() {
+#     local file=$1
+#     local destination=$2
+#     local full_file_path="$(dotfiles_location)/$file"
 
-  if [ ! -e "$destination" ]; then
-    echo "Symlinking $full_file_path -> $destination"
-    mkdir -p "$(dirname "$destination")"
-    ln -s "$full_file_path" "$destination"
-  fi
+#     if [ ! -e "$destination" ]; then
+#         echo "Symlinking $full_file_path -> $destination"
+#         mkdir -p "$(dirname "$destination")"
+#         ln -s "$full_file_path" "$destination"
+#     fi
+# }
+
+function dot_syml() {
+    local file=$1
+    local destination=$2
+    local force=$3
+    local full_file_path="$(dotfiles_location)/$file"
+
+    if [ ! -e "$destination" ]; then
+        echo "Symlinking $full_file_path -> $destination"
+        mkdir -p "$(dirname "$destination")"
+        ln -s "$full_file_path" "$destination"
+    else
+        if [[ "$force" == "F" ]]; then
+            echo "Symlinking (force) $full_file_path -> $destination"
+            ln -sFf "$full_file_path" "$destination"
+        fi
+    fi
 }
