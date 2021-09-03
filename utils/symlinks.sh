1f8d29a0ceb41e0e73dd46ca6ef035ae37bce70b
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-09-03 08:29:22 +0200

    improve symlink fn

--- utils/symlinks.sh
+++ utils/symlinks.sh
@@ -10,32 +10,25 @@ function dotfiles_location() {
     echo "$HOME/.dotfiles"
 }
 
-# function symlink_dotfile() {
-#     local file=$1
-#     local destination=$2
-#     local full_file_path="$(dotfiles_location)/$file"
-
-#     if [ ! -e "$destination" ]; then
-#         echo "Symlinking $full_file_path -> $destination"
-#         mkdir -p "$(dirname "$destination")"
-#         ln -s "$full_file_path" "$destination"
-#     fi
-# }
-
-function dot_syml() {
+# TODO: make sure you cannot symlink ~/
+function dot_symlink() {
     local file=$1
-    local destination=$2
+    local link_destination=$2
     local force=$3
-    local full_file_path="$(dotfiles_location)/$file"
+    local dotfiles_full_path="$(dotfiles_location)/$file"
 
-    if [ ! -e "$destination" ]; then
-        echo "Symlinking $full_file_path -> $destination"
-        mkdir -p "$(dirname "$destination")"
-        ln -s "$full_file_path" "$destination"
+    echo "dotfiles_full_path: $dotfiles_full_path"
+    echo "link_destination:   $link_destination"
+
+    if [ ! -e "$link_destination" ]; then
+        echo "Symlinking $dotfiles_full_path -> $link_destination"
+        mkdir -p "$(dirname "$link_destination")"
+        ln -sv "$dotfiles_full_path" "$link_destination"
     else
         if [[ "$force" == "F" ]]; then
-            echo "Symlinking (force) $full_file_path -> $destination"
-            ln -sFf "$full_file_path" "$destination"
+            echo "Symlinking (force) $dotfiles_full_path -> $link_destination"
+            # rm -rf $link_destination
+            ln -sv "$dotfiles_full_path" "$link_destination"
         fi
     fi
 }
