aca565d08a39b3748c876e30f617ff1070fe8ea1
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-09-03 08:49:29 +0200

    symlinks; indent fonts; minor

--- utils/symlinks.sh
+++ utils/symlinks.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 
-# _symlinks_current_dir="${BASH_SOURCE%/*}"
-
-# function dotfiles_location() {
-#   echo $(cd $_symlinks_current_dir/../.. && pwd)
-# }
-
 function dotfiles_location() {
     echo "$HOME/.dotfiles"
 }
 
-# TODO: make sure you cannot symlink ~/
 function dot_symlink() {
     local file=$1
     local link_destination=$2
     local force=$3
     local dotfiles_full_path="$(dotfiles_location)/$file"
 
-    echo "dotfiles_full_path: $dotfiles_full_path"
-    echo "link_destination:   $link_destination"
+
+    echo "file:                 $file"
+    echo "dotfiles_full_path:   $dotfiles_full_path"
+    echo "link_destination:     $link_destination"
+    # [ $file == "~" ] && exit 1 # lol this doesn't really work..
 
     if [ ! -e "$link_destination" ]; then
         echo "Symlinking $dotfiles_full_path -> $link_destination"
@@ -27,7 +23,7 @@ function dot_symlink() {
     else
         if [[ "$force" == "F" ]]; then
             echo "Symlinking (force) $dotfiles_full_path -> $link_destination"
-            # rm -rf $link_destination
+            rm -rf $link_destination # NOTE: be carefull!!!
             ln -sv "$dotfiles_full_path" "$link_destination"
         fi
     fi
