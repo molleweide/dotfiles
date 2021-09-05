387c188562cf6e7848b03643627f0d38dd1c609d
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-09-05 13:52:03 +0200

    more refactor and testing

--- lib/symlinks.sh
+++ lib/symlinks.sh
@@ -11,20 +11,20 @@ function dot_symlink() {
     local dotfiles_full_path="$(dotfiles_location)/$file"
 
 
-    echo "file:                 $file"
-    echo "dotfiles_full_path:   $dotfiles_full_path"
-    echo "link_destination:     $link_destination"
-    # [ $file == "~" ] && exit 1 # lol this doesn't really work..
+    # echo "file:                 $file"
+    # echo "dotfiles_full_path:   $dotfiles_full_path"
+    # echo "link_destination:     $link_destination"
+    # # [ $file == "~" ] && exit 1 # lol this doesn't really work..
 
     if [ ! -e "$link_destination" ]; then
         echo "Symlinking $dotfiles_full_path -> $link_destination"
         mkdir -p "$(dirname "$link_destination")"
-        ln -sv "$dotfiles_full_path" "$link_destination"
+        ln -s "$dotfiles_full_path" "$link_destination"
     else
         if [[ "$force" == "F" ]]; then
             echo "Symlinking (force) $dotfiles_full_path -> $link_destination"
             rm -rf $link_destination # NOTE: be carefull!!!
-            ln -sv "$dotfiles_full_path" "$link_destination"
+            ln -s "$dotfiles_full_path" "$link_destination"
         fi
     fi
 }
