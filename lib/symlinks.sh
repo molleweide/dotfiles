8912d722e732b5a50e836a875d34ad97d434f558
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-09-07 17:46:48 +0200

    syml

--- lib/symlinks.sh
+++ lib/symlinks.sh
@@ -2,8 +2,7 @@
 
 # NOTE: LIB/SYMLINKG
 #
-# - make sure I cannot overwrite anything important
-#
+# TODO: if regular file > prompt user overwrite??? yes/no
 
 function dotfiles_location() {
     echo "$HOME/.dotfiles"
@@ -16,11 +15,11 @@ function dot_symlink() {
     local dotfiles_full_path="$(dotfiles_location)/$file"
 
     if [ ! -e "$link_destination" ]; then
-        dotsay "@b@green[[ Symlink: $dotfiles_full_path -> $link_destination ]]"
+        dotsay "@b@green[[ Symlink: $file -> $link_destination ]]"
         mkdir -p "$(dirname "$link_destination")"
         ln -s "$dotfiles_full_path" "$link_destination"
     elif [[ -L $link_destination ]]; then
-        dotsay "@b@blue[[ Force re-link: $dotfiles_full_path -> $link_destination ]]"
+        dotsay "@b@blue[[ Force re-link: $file -> $link_destination ]]"
         rm -rf $link_destination # be carefull;
         ln -s "$dotfiles_full_path" "$link_destination"
     else
