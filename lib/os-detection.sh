c5698541224e9fc58c4122804981b226411e5d53
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-10-01 15:01:27 -0400

    save

--- lib/os-detection.sh
+++ lib/os-detection.sh
@@ -24,26 +24,3 @@ function is_apple_silicone() {
 function is_linux() {
     [[ "$_current_os" == "Linux" ]]
 }
-
-# https://en.wikipedia.org/wiki/X86-64
-# https://en.wikipedia.org/wiki/AArch64
-# https://askubuntu.com/a/1320647/22776
-# https://stackoverflow.com/q/67996157/130638
-
-function get_arch() {
-    if test "$arch" = 'aarch64' -o "$arch" = 'arm64'; then
-	    echo 'a64'  # raspberry pi, apple m1
-    elif [[ "$arch" = x86_64* ]]; then
-	    if [[ "$(uname -a)" = *ARM64* ]]; then
-		    echo 'a64'  # apple m1 running via `arch -x86_64 /bin/bash -c "uname -m"`
-	    else
-		    echo 'x64'
-	    fi
-    elif [[ "$arch" = i*86 ]]; then
-	    echo 'x32'
-    elif [[ "$arch" = arm* ]]; then
-	    echo 'a32'
-    else
-	    exit 1
-    fi
-}
