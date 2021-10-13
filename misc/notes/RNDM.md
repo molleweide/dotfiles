a417abfccbe2e995d7c8f7278d182c101d58308a
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2021-10-13 10:31:54 -0400

    save

--- misc/notes/RNDM.md
+++ misc/notes/RNDM.md
@@ -1,42 +1,12 @@
 # DOTFILES RNDM
 
-they hurt my feelings. no it was certainly not a common name. hjalle ska ha en balle mega sink dinkerink fink
-pinkelink maga fagga ragga dagga tagga num buck there is something that cannot be really done like jab or job which
-is not happening everywhere. if you got pharmacy get that mrna interchangeable blood cloth disorder which is fucking wierd and so
-that is not the reason why they did that which would be the best max as well as the data available in other countries
-and she's clearly awaiting that information. top off. daily beast brought to you buy who runs the daily beast it came
-out of the vanity fair I believe but it could also be another one. i am not a next focus on covid nineteen infectious
-disease ron clane and all kinds of goodies and the scenior institute for development and his first name which is
-anahci. fauci frau freu missantropical bombergranades with scenior kurtzentaag and make money its true dude and so that is
-quite fucking cool. as we have everything narrative past couple of days not to do with the boosters.
+so this is quite interesting and important actually and so we are going to see what this gives us now.
 
-title changes seth ducass good morning a donation for my smoking hot husband seth the motherfucking shit ass
-cunt fucker. so that is not what I want you know. sounds like a recipe for success to me.
+so this is actually quite fucking insane that it can be this fucking stupid and so this is something that I don't really
+want and so that is the reason why they made it like that in the end.
 
-i should later do a test of the apple silicone script on my main computer which is going to be fucking insane. and so that is qu
-quite fucking insane and so I am going to fucking do the best thing dababy punched the lgbtq community right in the aids.
-which is quite fucking funny. but also that is something that is very interesting and so that is what really does it for me.
+this is going to be fucking amazing and so that is not something that I would like to do so this is something that 
+is the biggest thing ever and so that is the best thing ever. i really need to do this fucking shit.
 
-i just cannot fucking understand how that could ever be the case and so now. with the tiktok trend and the algo
-in tiktok is chinese so it is really wierd how they can allow tiktok to be allowed on the american market. it is actually
-quite fucking insane. it is stealth scalp popping and you pull really hard. and so this would be something that you could do.
-and so this is quite insane actually. between my different macbooks i have different hardware keyboards and then I have 
-a couple of different qmk keyboards and this is insane how well this works. this makes it so that you only have to
-create a defsrc layout and then you can have a single layout file where you maintain your actual custom layout.
-
-this works flawlessly for myself but I need more testing for this. a couple things to catch up on. in the media
-this is something that the media is not touching upon at all. nice this is fucking nice dude and so that is something
-that is quite fucking insane.
-
-it is bringing back a tiny bit of ratings because he supposedly said something that were quite fucking crazy
-when he came on late at night because you know he was watching which was insane.
-
-killing twelve year olds so i sayhey every body. i want you to kill them. and show this is not a joke. so this is not
-something that is going to work. and this is going to be a life long habit that I am build ing now.
-if i get a bit more comfortable now i a mgoing to become fucking insane when it comes to productivity because
-this setup with vim is quite fucking insane and ita llows for some insanes setups and this is the best fucking thing
-ever. that is fucking pathetic. it is training for retards. which is quite fucking insane. and so that is not good at all.
-so the stronges my grip gets the better this is going to get. it generates torque that is great.
-i just don't like him because he doesn't take torque, coordination i just moved here. so that is why i don't like it.
-i just do as much as possible which is quite fucking insane. and so that is quite fucking nice. i did a long walk with
-it and si so that you are being very forgiving with that outfit my friend.
+there is no space for hate speech on youtube and so we are going to support communities that work against this 
+movement or tendency.
--- sources/generic.bash
+++ sources/generic.bash
@@ -7,27 +7,27 @@
 
 if "$DOROTHY/commands/is-mac"; then
 	export USER_SHELLS=(
-	    bash
-	    zsh
-		fish
-		sh
-	)
+	bash
+	zsh
+	fish
+	sh
+)
 else
 	export USER_SHELLS=(
-		bash
-		zsh
-		fish
-		sh
-	)
+	bash
+	zsh
+	fish
+	sh
+)
 fi
 
 export APK_INSTALL=(
-	# gocryptfs: use `setup-util-gocryptfs` instead, as this version lags behind
-	aria2
-	git
-	gnupg
-	tree
-	wget
+# gocryptfs: use `setup-util-gocryptfs` instead, as this version lags behind
+aria2
+git
+gnupg
+tree
+wget
 )
 
 # export APT_REMOVE=(
@@ -44,152 +44,207 @@ export APK_INSTALL=(
 # )
 
 export APT_ADD=(
-	# software-properties-common
-	aria2
-	atomicparsley # a youtube-dl dependency
-	build-essential
-	curl
-	git
-	gnome-keyring
-	libnotify-bin
-	libssl-dev
-	make
-	openssl
-	traceroute
-	vim
+# software-properties-common
+aria2
+atomicparsley # a youtube-dl dependency
+build-essential
+curl
+git
+gnome-keyring
+libnotify-bin
+libssl-dev
+make
+openssl
+traceroute
+vim
 )
 
 # https://code.visualstudio.com/docs/setup/linux
 # if [[ "$(get-arch)" == 'x'* ]]; then
 export SNAP_INSTALL=(
-	# 'code --classic'
-	# deno
+# 'code --classic'
+# deno
 )
 
 if "$DOROTHY/commands/is-mac"; then
 	export GO_INSTALL=(
-		changkun.de/x/rmtrash
-		# github.com/cloudflare/utahfs/cmd/utahfs-client
-	)
+	changkun.de/x/rmtrash
+	# github.com/cloudflare/utahfs/cmd/utahfs-client
+)
 else
 	export GO_INSTALL=(
-		changkun.de/x/rmtrash
-		# github.com/rfjakob/gocryptfs: use `setup-util-gocryptfs` instead, as this version lags behind
-	)
+	changkun.de/x/rmtrash
+	# github.com/rfjakob/gocryptfs: use `setup-util-gocryptfs` instead, as this version lags behind
+)
 fi
 
 # export HOMEBREW_ARCH="x86_64"
 export HOMEBREW_INSTALL=(
-	# azure-cli
-	# balena-cli
-	# blackbox
-	# hashicorp/tap/boundary
-	# hashicorp/tap/consul
-	# hashicorp/tap/nomad
-	# hashicorp/tap/terraform
-	# hashicorp/tap/vault
-	# heroku
-	# kryptco/tap/kr
-	# yarn
-	aria2
-	coreutils
-	deno
-	git
-	git-extras
-	git-lfs
-	ImageMagick
-	mas
-	micro
-	openssh
-	pkg-config
-	podman
-	screen
-	terminal-notifier
-	tmux
-	tree
-	vim
-	watch
-	watchman
-	wget
+    # azure-cli
+    # balena-cli
+    # blackbox
+    # hashicorp/tap/boundary
+    # hashicorp/tap/consul
+    # hashicorp/tap/nomad
+    # hashicorp/tap/terraform
+    # hashicorp/tap/vault
+    # heroku
+    # kryptco/tap/kr
+    # yarn
+    neomutt
+    curl
+    isync
+    msmtp
+    pass
+    spotify-tui
+    mps-youtube
+    aria2
+    coreutils
+    deno
+    gpg
+    pass
+    passmenu
+    pandoc
+    # librsvg
+    # basictex
+    ffmpeg
+    sox
+    ranger              # filebrowser
+    nnn                 # filebrowser
+    lf                  # filebrowser
+    fff                 # filebrowser
+    cointop
+    dmenu-mac
+    figlet              # http://www.figlet.org/examples.html
+    pass-otp
+    gpg-tui
+    diff-so-fancy
+    lazygit
+    qtpass
+    pinentry-mac
+    sc-im               # spreadsheets
+    git
+    git-extras
+    git-lfs
+    ImageMagick
+    mas
+    tg                  # telegram tui
+    calcurse
+    micro
+    openssh
+    pkg-config
+    podman
+    screen
+    terminal-notifier
+    tmux
+    tree
+    vim
+    watch
+    watchman
+    wget
+    ack
+    blueutil
+    code-minimap
+    coreutils
+    fd
+    gh
+    gnu-tar
+    heroku/brew/heroku
+    hub
+    llvm
+    most
+    postgres
+    readline
+    redis
+    ripgrep
+    sl
+    the_silver_searcher
+    universal-ctags
+    wget
+    wp-cli
+    xz # general-purpose data compression with high compression ratio
 )
 export HOMEBREW_INSTALL_SLOW=(
-	gpg
-	shellcheck
+gpg
+shellcheck
 )
 export HOMEBREW_INSTALL_CASK=(
-	# 1password-cli
-	# acorn
-	# adguard
-	# airparrot
-	# appzapper
-	# atom
-	# audio-hijack
-	# backblaze
-	# bartender
-	# brave
-	# burn
-	# caption
-	# ccleaner
-	# contexts
-	# dat
-	# devdocs
-	# fantastical
-	# firefox
-	# freedom
-	# geekbench
-	# github-desktop
-	# gitter
-	# google-chrome
-	# google-hangouts
-	# jaikoz
-	# java
-	# julia
-	# keybase
-	# kodi
-	# little-snitch
-	# loopback
-	# micro-snitch
-	# numi
-	# opera
-	# paragon-ntfs
-	# plex-media-server
-	# pomello
-	# reflector
-	# screenflow
-	# signal
-	# sketch
-	# skype
-	# soundsource
-	# spotify
-	# teamviewer
-	# toggldesktop
-	# torbrowser
-	# tower
-	# transmission
-	# transmit
-	# tunnelbear
-	# tunnelblick
-	# ubersicht
-	# undercover
-	# usage
-	# visual-studio-code
-	# vlc
-	# vmware-fusion
-	# webtorrent
-	# windscribe
-	# workflowy
-	# xld
-	calibre
+# 1password-cli
+# acorn
+# adguard
+# airparrot
+# appzapper
+# atom
+brew_cask_install aerial # screensaver
+# audio-hijack
+# backblaze
+# bartender
+brave
+# burn
+# caption
+# ccleaner
+# contexts
+# dat
+# devdocs
+# fantastical
+# firefox
+# freedom
+# geekbench
+# github-desktop
+# gitter
+# google-chrome
+# google-hangouts
+# jaikoz
+# java
+# julia
+# keybase
+# kodi
+# little-snitch
+# loopback
+# micro-snitch
+# numi
+# opera
+# paragon-ntfs
+# plex-media-server
+# pomello
+# reflector
+# screenflow
+# signal
+# chromedriver
+# sketch
+# skype
+# soundsource
+spotify
+supercollider       # audio engine
+# teamviewer
+# toggldesktop
+# torbrowser
+# tower
+# transmission
+# transmit
+# tunnelbear
+# tunnelblick
+# ubersicht
+# undercover
+# usage
+# visual-studio-code
+# vlc
+# vmware-fusion
+# webtorrent
+# windscribe
+# workflowy
+# xld
+# calibre
 )
 
 # gems
 export RUBY_VERSION="2.7"
 export RUBY_INSTALL=(
-	compass
-	ffi
-	git-up
-	sass
-	twurl
+compass
+ffi
+git-up
+sass
+twurl
 )
 
 # pip
@@ -199,9 +254,9 @@ export RUBY_INSTALL=(
 # - https://github.com/pikepdf/pikepdf
 # - https://github.com/pdfminer/pdfminer.six
 export PYTHON_INSTALL=(
-	# bitcoinlib
-	# cairosvg
-	# PyPDF2  # python 2 only
+# bitcoinlib
+# cairosvg
+# PyPDF2  # python 2 only
 )
 if "$DOROTHY/commands/is-ubuntu"; then
 	PYTHON_INSTALL+=('youtube_dl')
@@ -209,14 +264,14 @@ fi
 
 # npm/yarn
 export NODE_INSTALL=(
-	# bevry
-	"@bevry/testen"
-	boundation
+# bevry
+# "@bevry/testen"
+# boundation
 
 	# servers
 	# browser-refresh
 	# live-server
-	serve
+	# serve
 
 	# database
 	# fauna-shell
@@ -250,35 +305,81 @@ export NODE_INSTALL=(
 # rust/cargo/crates.io
 # --locked: Require Cargo.lock is up to date
 # as rust packages can be supported by many package managers, and as cargo requires downloading every dependency, most rust packages are better installed via SETUP_UTILS
-export RUST_INSTALL=()
+export RUST_INSTALL=(
+ttyper
+)
 
 # utilities
 # setup-util-*
 export SETUP_UTILS=(
-	bandwich
-	bash
-	bat
-	bottom
-	delta
-	dust
-	exa
-	fd
-	fish
-	gh
-	gocryptfs
-	grex
-	httpie
-	hyperfine
-	jq
-	procs
-	ripgrep
-	rmesg
-	sd
-	tealdeer
-	tokei
-	zoxide
+bandwich
+bash
+bat
+bottom
+delta
+dust
+exa
+fd
+fish
+gh
+gocryptfs
+grex
+httpie
+hyperfine
+jq
+procs
+ripgrep
+rmesg
+sd
+tealdeer
+tokei
+z
+zoxide
 )
 
 # TODO:
-export MAC_APPS_INSTALL=()
+# export MAC_APPS_INSTALL=(
+#     mas_install "Amphetamine" 937984704
+#     mas_install "Blackmagic Disk Speed Test" 425264550
+#     mas_install "Boom 2" 948176063
+#     mas_install "Capo 3" 696977615
+#     mas_install "Fantastical 2" 975937182
+#     mas_install "GarageBand" 682658836
+#     mas_install "Logic Pro X" 634148309
+#     mas_install "Markoff" 1084713122
+#     mas_install "Paste 2" 967805235
+#     mas_install "Pixelmator" 407963104
+#     mas_install "Slack" 803453959
+#     mas_install "Timeout" 402592703
+#     mas_install "The Unarchiver" 425424353
+# )
 
+# heroku plugins:install heroku-repo
+# # cargo install --git https://github.com/boxdot/gurk-rs
+# # brew install irssi
+# # brew tap paul-nameless/homebrew-repo
+
+# brew tap homebrew/cask-fonts # if ! brew_is_tapped "neovim/neovim"; then
+# brew install font-inconsolata-go-nerd-font
+# brew install font-jetbrains-mono-nerd-font
+
+# brew install --cask mpv
+
+# brew install exa
+# brew install neofetch
+# brew install zsh
+# brew install zsh-completions
+# brew install direnv
+# # brew_install z # would work better with neovim telescope!
+# brew install fasd
+# brew install fzf
+# # if [ ! -f $HOME/.fzf.zsh ]; then
+# #     $(brew --prefix)/opt/fzf/install --no-update-rc --key-bindings --completion
+# # fi
+
+# # # install antigen plugin manager
+# # antigen_file="${XDG_CONFIG_HOME:-$HOME/.config}/zsh/vendor/antigen.zsh"
+# # if [ ! -f $antigen_file ]; then
+# #     echo "Installing antigen"
+# #     curl -L git.io/antigen > "$antigen_file"
+# # fi
