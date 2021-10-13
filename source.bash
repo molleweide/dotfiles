d98f867fc15734c33e681f64f6ababda631b3b55
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2021-10-13 02:21:54 -0400

    doro user first

---
+++ source.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# dorothy configuration options:
+# https://github.com/bevry/dorothy#configuration
+# https://github.com/bevry/dorothy/blob/master/sources/defaults.bash
+
+# inherit the cross-platform shell configuration
+source "$DOROTHY/user/source.sh"
+
---
+++ source.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# this file must be compatible with fish, zsh, bash
+
+# dorothy configuration options:
+# https://github.com/bevry/dorothy#configuration
+# https://github.com/bevry/dorothy/blob/master/sources/defaults.sh
+
+# source my env file
+. "$DOROTHY/user/env.sh"
+
