a7b5a0423e02920c2dead49e9d03bf645b22b51d
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2021-11-14 04:34:45 -0500

    update and clean up my aliases. many goodies

--- sources/aliases.sh
+++ sources/aliases.sh
@@ -1,12 +1,13 @@
-# SHELL
+#---------------------------
+#---       general       ---
+#---------------------------
+
 alias ralias="source $DOROTHY/user/sources/aliases.sh"
 alias rl="ralias"
-
 alias xxx="exec zsh"
 alias k="clear"
 alias mm="man man"
 alias tt="ttyper"
-
 # alias dbm="rake db:migrate && RAILS_ENV=test rake db:migrate"
 alias j=z # I'm used to autojump 'j' vs fasd 'z'
 alias less="less -r"
@@ -14,29 +15,51 @@ alias less="less -r"
 # alias srync="rsync -vrazh"
 # alias emacs="TERM=xterm-24bit emacs -nw"
 alias cl="calcurse"
-
+alias ei="cointop"
 alias duu="diskutil" # list commands
 alias dul="diskutil list" # list drives
+alias tst="testing-shell"
+alias ctgr="ctags -R"
+# # shortcuts
+# alias vi='vim'
+# alias nv='nvim'
+# alias afind='ack -il'
+alias md='mkdir -p'
+alias rd=rmdir
+# alias please=sudo
+alias po=popd
+alias pu=pushd
+# alias globurl='noglob urlglobber '
+alias ZZ=exit
 
-# DOROTHY =============================================
+#---------------------------
+#---       DOROTHY       ---
+#---------------------------
 
-# DOROTHY USER ==========================================
+alias dorp="setup-dorothy-permissions"
+
+# USER
 
 alias pstf="setup-personal-repos"
 alias suc="setup-user-configs"
 
 # ctags ==========================================
-alias ctgr="ctags -R"
 
-# ======== TILING ========
-alias rst="reload_tiling"
+#--------------------------
+#---       TILING       ---
+#--------------------------
+
 function reload_tiling() {
     brew services restart yabai
     brew services restart skhd
     limelight # there is a wierd err msg but it works...
 }
 
-# ======== FIND FILES ========
+alias rtile="reload_tiling"
+
+#--------------------------
+#---       locate       ---
+#--------------------------
 
 # https://egeek.me/2020/04/18/enabling-locate-on-osx/
 if which glocate > /dev/null; then
@@ -45,179 +68,20 @@ if which glocate > /dev/null; then
 fi
 alias loaddb="gupdatedb --localpaths=$HOME --prunepaths=/Volumes --output=$HOME/locatedb"
 
+#--------------------------------
+#---       SPREADSHEETS       ---
+#--------------------------------
 
-# ======== FINANCE ===========
-alias ei="cointop"
-
-
-# ======== SPREADSHEET ===========
 alias scm="sc-im"
-alias scml=~/.local/bin/sc-im
+alias scml="~/.local/bin/sc-im"
 
-
-# tmux
-alias tm="tmux"
-alias tk="tmux kill-session"
-alias tn="tmuxinator"
-
-# ======== DOCKER ===========
-
-# ======== FILE MANAGERS ===========
-
-alias fh="fff"
-alias fj="lf"
-alias fk="ranger"
-alias fn="nnn"
-
-function ranger {
-    local IFS=$'\t\n'
-    local tempfile="$(mktemp -t tmp.XXXXXX)"
-    local ranger_cmd=(
-        command
-        ranger
-        --cmd="map Q chain shell echo %d > "$tempfile"; quitall"
-    )
-
-    ${ranger_cmd[@]} "$@"
-    if [[ -f "$tempfile" ]] && [[ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]]; then
-        cd -- "$(cat "$tempfile")" || return
-    fi
-    command rm -f -- "$tempfile" 2>/dev/null
-}
-
-# ======== VIM ALIASES ===========
-
-alias vim="nvim"
-alias v="nvim"
-alias vf="nvim -t" # search for tag
-alias vr="nvim -R"
-alias vh="nvim -headless"
-alias vNN="nvim -u NONE"
-alias vNC="nvim -u NORC"
-alias vh="nvim -headless"
-alias vp="nvim --cmd \"set rtp+=\$(pwd)\""
-
-# my fork
-alias vv="~/code/neovim/build/bin/nvim" # forked build
-
-# ======== GIT ========
-alias gs='git status -sb'
-alias gcfls="git conflicts"
-alias m="git checkout molleweide"
-alias M="git checkout molldev"
-alias gcreb="git add --all && git commit -m 'REBASE ME!!!'"
-alias gcun="git add notes/ && git commit -m 'user(molleweide): notes'"
-alias sv="git add --all && git commit -m 'save'"
-alias svp="git add --all && git commit -m 'save' && git push"
-alias frk="gh repo fork"
-alias devpr="gh pr create --base develop"
-alias lg="lazygit"
-
-# 1 = branch, 2 = sub path, 3 = new repo url
-alias gbrkout="git-subdir-make-into-module"
-alias gsmv="git-submodule-mv"
-
-# CHAT =========================================
-alias sg="gurk" # signal
-# alias sg="gurk" # siggo has better support than gurk
-alias nt="neomutt" # email
-# tg for telegram is default..
-# discord
-
-# ======== QMK ================================
-# TODO: add qmk dir to environment
-alias tfl="teensy_loader_cli -mmcu=atmega32u4 -w ergodox_ez_molleweide.hex"
-alias qmkflash="cd $HOME/code/firmware/qmk-firmware/ && tfl"
-
-
-# ======== DOCKER ====================================
-alias dk="docker"
-alias dkc="docker-compose"
-alias dkwp="docker-compose run --rm wpcli"
-
-# ======== KARABINER ========================================
-alias karb="/Library/Application\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli"
-alias krb="open -a /Applications/Karabiner-Elements.app"
-
-# ======== KMONAD ============================================
-# TODO: add kmonad config to environment
-alias keyb="$XDG_CONFIG_HOME/kmonad/keyboard"
-kmonad=~/.local/bin/kmonad
-layouts=~/code/tools/kmonad/keymap/user/molleweide
-kmopro()  { sudo $kmonad $layouts/macbook_pro_2012.kbd; }
-kmoair()  { sudo $kmonad $layouts/macbook_air_2021_m1.kbd; }
-kmoez()   { sudo $kmonad $layouts/ergodox_ez.kbd; }
-kmoproT() { sudo $kmonad $layouts/macbook_pro_2012.kbd      -l debug; }
-kmoairT() { sudo $kmonad $layouts/macbook_air_2021_m1.kbd   -l debug; }
-kmoezT()  { sudo $kmonad $layouts/ergodox_ez.kbd            -l debug; }
-
-
-# ======== GAMES ========
-
-# ======== MUSIC & VIDEO ========
-alias yt="mpsyt"
-
-# ======== GPG ========
-alias gpglk="gpg --list-keys"
-alias gpglkp="gpg --list-public-keys"
-alias gpglks="gpg --list-secret-keys"
-alias gpge="gpg --edit-keys"
-
-# ======== HASKELL ===========
-alias rh="runhaskell"
-
-
-# OS X apps
-alias md="open -a Markoff $@"
-
-# Git
-function fco() {
-  git checkout $(git branch -a --sort=-committerdate | \
-      cut -c 3- | \
-      sed 's/^remotes\/[^/]*\///' | \
-      sort | \
-      uniq | \
-      grep -v HEAD | \
-      fzf-tmux -d 20)
-}
-
-function pgrefresh() {
-  rm -fr /usr/local/var/postgres/postmaster.pid
-  brew services restart postgresql
-}
-
-function sslcert() {
-  echo | \
-    openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | \
-    openssl x509 -inform pem -noout -text
-}
-
-function cheat() {
-  curl cht.sh/$1
-}
-
-function opendir() {
-  local file="$1"
-  local dir=$(dirname "$file")
-
-  open "$dir"
-}
-
-function krbcp() {
-  # this works but if can take a while before the settings update.
-  # force update by deselecting and then selecting the mod again.
-  make
-  cp public/json/molleweide.json ~/.config/karabiner/assets/complex_modifications
-}
-
-## FZF FUNCTIONS ## OLD START ##
+#---------------------------------------
+#---       SEARCH & FIND FILES       ---
+#---------------------------------------
 
 # fo [FUZZY PATTERN] - Open the selected file with the default editor
 #   - Bypass fuzzy finder if there's only one match (--select-1)
 #   - Exit if there's no match (--exit-0)
-#
-#   this is how you create functions in/for the terminal
-#     see keycommands.md
 fo() {
   local files
   IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
@@ -229,15 +93,22 @@ fz() {
   print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
 }
 
-# fbr [FUZZY PATTERN] - Checkout specified branch
-# Include remote branches, sorted by most recent commit and limited to 30
-fgb() {
-  local branches branch
-  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
-  branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+# fuzzy grep via rg and open in vim with line number
+fgr() {
+  local file
+  local line
+
+  read -r file line <<<"$(rg --no-heading --line-number $@ | fzf -0 -1 | awk -F: '{print $1, $2}')"
+
+  if [[ -n $file ]]
+  then
+     vim $file +$line
+  fi
 }
+
+# ------------------------
+# ---       TMUX       ---
+# ------------------------
 
 # tm [SESSION_NAME | FUZZY PATTERN] - create new tmux session, or switch to existing one.
 # Running `tm` will let you fuzzy-find a session mame
@@ -260,48 +131,228 @@ ftmk() {
   session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux kill-session -t "$session" || echo "No session found to delete."
 }
 
-# fuzzy grep via rg and open in vim with line number
-fgr() {
-  local file
-  local line
 
-  read -r file line <<<"$(rg --no-heading --line-number $@ | fzf -0 -1 | awk -F: '{print $1, $2}')"
+alias tm="tmux"
+alias tk="tmux kill-session"
+alias tn="tmuxinator"
 
-  if [[ -n $file ]]
-  then
-     vim $file +$line
-  fi
+
+#---------------------------
+#---       dockers       ---
+#---------------------------
+
+alias dk="docker"
+alias dkc="docker-compose"
+alias dkwp="docker-compose run --rm wpcli"
+
+#---------------------------------
+#---       file browsers       ---
+#---------------------------------
+
+alias fh="fff"
+alias fj="lf"
+alias fk="ranger"
+alias fn="nnn"
+
+# switch cwd on exit
+function ranger {
+    local IFS=$'\t\n'
+    local tempfile="$(mktemp -t tmp.XXXXXX)"
+    local ranger_cmd=(
+        command
+        ranger
+        --cmd="map Q chain shell echo %d > "$tempfile"; quitall"
+    )
+
+    ${ranger_cmd[@]} "$@"
+    if [[ -f "$tempfile" ]] && [[ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]]; then
+        cd -- "$(cat "$tempfile")" || return
+    fi
+    command rm -f -- "$tempfile" 2>/dev/null
 }
 
-## FZF FUNCTIONS ## OLD END ##
+#-----------------------
+#---       VIM       ---
+#-----------------------
 
+alias vim="nvim"
+alias v="nvim"
+alias vv="~/code/neovim/build/bin/nvim" # forked build
+alias vf="nvim -t" # search for tag
+alias vr="nvim -R"
+alias vh="nvim -headless"
+alias vNN="nvim -u NONE"
+alias vNC="nvim -u NORC"
+alias vh="nvim -headless"
+alias vp="nvim --cmd \"set rtp+=\$(pwd)\""
+
+#-----------------------
+#---       GIT       ---
+#-----------------------
+
+function fco() {
+  git checkout $(git branch -a --sort=-committerdate | \
+      cut -c 3- | \
+      sed 's/^remotes\/[^/]*\///' | \
+      sort | \
+      uniq | \
+      grep -v HEAD | \
+      fzf-tmux -d 20)
+}
+
+# fbr [FUZZY PATTERN] - Checkout specified branch
+# Include remote branches, sorted by most recent commit and limited to 30
+fgb() {
+  local branches branch
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
+alias gs='git status -sb'
+alias gcfls="git conflicts"
+alias m="git checkout molleweide"
+alias M="git checkout molldev"
+alias gcreb="git add --all && git commit -m 'REBASE ME!!!'"
+alias gcun="git add notes/ && git commit -m 'user(molleweide): notes'"
+alias sv="git add --all && git commit -m 'save'"
+alias svp="git add --all && git commit -m 'save' && git push"
+alias frk="gh repo fork"
+alias devpr="gh pr create --base develop"
+alias lg="lazygit"
+
+# 1 = branch, 2 = sub path, 3 = new repo url
+alias gbrkout="git-subdir-make-into-module"
+alias gsmv="git-submodule-mv"
+
+#----------------------------------
+#---       COMMUNICATIONS       ---
+#----------------------------------
+
+alias sg="gurk" # signal
+# alias sg="gurk" # siggo has better support than gurk
+alias nt="neomutt" # email
+# tg for telegram is default..
+# discord
+
+#-----------------------
+#---       QMK       ---
+#-------------------------
+
+# TODO: add qmk dir to environment
+alias tfl="teensy_loader_cli -mmcu=atmega32u4 -w ergodox_ez_molleweide.hex"
+alias qmkflash="cd $HOME/code/firmware/qmk-firmware/ && tfl"
+
+#-----------------------------
+#---       KARABINER       ---
+#----------------------------
+
+function krbcp() {
+  # this works but if can take a while before the settings update.
+  # force update by deselecting and then selecting the mod again.
+  make
+  cp public/json/molleweide.json ~/.config/karabiner/assets/complex_modifications
+}
+
+alias karb="/Library/Application\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli"
+alias krb="open -a /Applications/Karabiner-Elements.app"
+
+#--------------------------
+#---       KMONAD       ---
+#-------------------------
+
+kmonad=~/.local/bin/kmonad
+layouts=~/code/tools/kmonad/keymap/user/molleweide
+
+alias kmo="$XDG_CONFIG_HOME/kmonad/keyboard"
+
+# old
+kmopro()  { sudo $kmonad $layouts/macbook_pro_2012.kbd; }
+kmoair()  { sudo $kmonad $layouts/macbook_air_2021_m1.kbd; }
+kmoez()   { sudo $kmonad $layouts/ergodox_ez.kbd; }
+kmoproT() { sudo $kmonad $layouts/macbook_pro_2012.kbd      -l debug; }
+kmoairT() { sudo $kmonad $layouts/macbook_air_2021_m1.kbd   -l debug; }
+kmoezT()  { sudo $kmonad $layouts/ergodox_ez.kbd            -l debug; }
+
+#-------------------------
+#---       MEDIA       ---
+#-------------------------
+
+alias yt="mpsyt"
+
+#-----------------------
+#---       GPG       ---
+#-----------------------
+
+alias gpglk="gpg --list-keys"
+alias gpglkp="gpg --list-public-keys"
+alias gpglks="gpg --list-secret-keys"
+alias gpge="gpg --edit-keys"
+
+#---------------------------
+#---       HASKELL       ---
+#---------------------------
+
+alias rh="runhaskell"
+
+#----------------------------
+#---       OSX ---
+#----------------------------
 
 slp() {
   osascript -e 'tell application "Finder" to sleep'
 }
 
+#------------------------------
+#---       MACOS APPS       ---
+#------------------------------
+
+# OS X apps
+alias md="open -a Markoff $@"
+
+
+#--------------------------
+#---       OTHERS       ---
+#-------------------------
+
+function pgrefresh() {
+  rm -fr /usr/local/var/postgres/postmaster.pid
+  brew services restart postgresql
+}
+
+function sslcert() {
+  echo | \
+    openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | \
+    openssl x509 -inform pem -noout -text
+}
+
+function cheat() {
+  curl cht.sh/$1
+}
+
+function opendir() {
+  local file="$1"
+  local dir=$(dirname "$file")
+  open "$dir"
+}
 
 
 
-
-#############################################################
-#############################################################
-#############################################################
-#############################################################
-#############################################################
-#############################################################
-#############################################################
+#---------------------------------
+#---       OTHER ALIASES       ---
+#--------------------------------
 
 
 
-
-
-# ### aliases ---------------------------------------------------------------- {{{
 # # https://medium.com/@webprolific/getting-started-with-dotfiles-43c3602fd789#.vh7hhm6th
 # # https://github.com/webpro/dotfiles/blob/master/system/.alias
 # # https://github.com/mathiasbynens/dotfiles/blob/master/.aliases
 
-# zsh only aliases
+#----------------------------
+#---       ZSH ONLY       ---
+#----------------------------
+
 if [[ -n ${ZSH_VERSION-} ]]; then
   alias 1='cd -'
   alias 2='cd -2'
@@ -356,18 +407,6 @@ fi
 alias d='dirs -v | head -10'
 # alias v='vim'
 alias h='history'
-
-# # shortcuts
-# alias vi='vim'
-# alias nv='nvim'
-# alias afind='ack -il'
-alias md='mkdir -p'
-alias rd=rmdir
-# alias please=sudo
-alias po=popd
-alias pu=pushd
-# alias globurl='noglob urlglobber '
-alias ZZ=exit
 
 # # more ways to ls
 alias ll='ls -lFh'
