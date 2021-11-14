a75b508c977472fa5db0262b3c2cb1d6e8b049c2
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2021-11-14 04:37:45 -0500

    save

--- sources/setopts.zsh
+++ sources/setopts.zsh
@@ -11,7 +11,7 @@ unsetopt PROMPT_SP # i believe this removes ^% sign in prompt
 
 # # Changing Directories
 # # http://zsh.sourceforge.net/Doc/Release/Options.html#Changing-Directories
-# setopt auto_cd                 # if a command isn't valid, but is a directory, cd to that dir
+setopt auto_cd                 # if a command isn't valid, but is a directory, cd to that dir
 # setopt auto_pushd              # make cd push the old directory onto the directory stack
 # setopt pushd_ignore_dups       # don’t push multiple copies of the same directory onto the directory stack
 # setopt pushd_minus             # exchanges the meanings of ‘+’ and ‘-’ when specifying a directory in the stack
