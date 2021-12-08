fa9c7218c0f4ef33ef458856ed9d529e365492be
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2021-12-08 09:13:40 -0500

    save

--- config/ddns.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck disable=SC2034
-# do not use `export` keyword in this file
-
-DDNS_URL=''
-DDNS_WORKER=''
-DDNS_WORKER_AUTH=''
--- config/nofap.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck disable=SC2034
-# do not use `export` keyword in this file
-
-export NOFAP_DISCORD_WEBHOOK=''
