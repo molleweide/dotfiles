ee8bb13cf9d0beb7cfafe6b623374874e768a32e
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2021-12-21 07:02:32 -0500

    save

--- sources/aliases.sh
+++ sources/aliases.sh
@@ -253,23 +253,23 @@ function ranger {
     command rm -f -- "$tempfile" 2>/dev/null
 }
 
-#-----------------------
-#---       VIM       ---
-#-----------------------
+# #-----------------------
+# #---       VIM       ---
+# #-----------------------
 
-# Use neovim for vim if present.
-[ -x "$(command -v nvim)" ] && alias vim="nvim" vimdiff="nvim -d"
+# # Use neovim for vim if present.
+# [ -x "$(command -v nvim)" ] && alias vim="nvim" vimdiff="nvim -d"
 
-alias vim="nvim"
-alias v="nvim"
-alias vv="~/code/neovim/build/bin/nvim" # forked build
-alias vf="nvim -t" # search for tag
-alias vr="nvim -R"
-alias vh="nvim -headless"
-alias vNN="nvim -u NONE"
-alias vNC="nvim -u NORC"
-alias vh="nvim -headless"
-alias vp="nvim --cmd \"set rtp+=\$(pwd)\""
+# alias vim="nvim"
+# alias v="nvim"
+# alias vv="~/code/neovim/build/bin/nvim" # forked build
+# alias vf="nvim -t" # search for tag
+# alias vr="nvim -R"
+# alias vh="nvim -headless"
+# alias vNN="nvim -u NONE"
+# alias vNC="nvim -u NORC"
+# alias vh="nvim -headless"
+# alias vp="nvim --cmd \"set rtp+=\$(pwd)\""
 
 #-----------------------
 #---       GIT       ---
