a0b29af15c6b630675c3707165cf42d294b7b337
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2023-06-02 09:37:28 -0400

    save

---
+++ config.local/environment.bash
@@ -0,0 +1,6 @@
+# STRONGBOX ENCRYPTED RESOURCE ; See https://github.com/uw-labs/strongbox
+PirkCe92w383Oqy1W48cL5HJzJQwZk6uwt9o6N9FWBrfDo2XZIq8dgYcy+UN5vbxdm0d/BCY5ENz
+CSDz9adJLM6CRt7ySoB3CfQnQJQddOn8e9MDgsaeRoo2MlMTWbLJgCcxyqvqfldC8kJRMoFoPR06
+QUsIldrZDXU35goAUux9ESs9wRCjYsKePhhkuDgt0vjVgoAjCrtmheozRbzZxo5BLHC/yUw+Qt+v
+DyTmRnBX9RkWZkv+K/8XcqOYKXbtMJbTj4GnooXhp8A2ZJ09BR3k0QiI5C9TwEfoON/W9b/wFBbj
+3Fz59iSlUWOppHMxe/ZAferFqpIsNViK4A==
