6c4ec02127a13995c7f039699875dca5f78b53d6
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2024-09-26 04:46:56 -0400

    save. improve ghm

--- config.xdg/qutebrowser/config.py
+++ config.xdg/qutebrowser/config.py
@@ -12,7 +12,7 @@
 
 ## This is here so configs done via the GUI are still loaded.
 ## Remove it to not load settings done via the GUI.
-# config.load_autoconfig(True)
+config.load_autoconfig(False)
 
 ## Aliases for commands. The keys of the given dictionary are the
 ## aliases, while the values are the commands they map to.
