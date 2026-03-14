ba2f0c35910c641535943305def7c31a8be8e84f
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2026-03-14 10:54:20 +0100

    add my oz tweaks to themes

---
+++ themes/oz2.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+
+function fish_prompt
+	set last_command_exit_status "$status"
+	if test ! -d "$DOROTHY"
+		printf '%s\n' 'DOROTHY has been moved, please re-open your shell' >&2
+		return 1
+	end
+	"$DOROTHY/themes/oz" fish "$last_command_exit_status" "$FISH_VERSION"
+end
